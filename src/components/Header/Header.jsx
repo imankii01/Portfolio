789e787d0590996504771c0bc71f8e94938c9230
Ankit <codewithankit047@gmail.com>
2023-01-26 23:12:35 +0530

    heder top cmmit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,64 +1,60 @@
-import React from 'react';
-import './header.css';
+import React from "react";
+import "./header.css";
 
 const Header = () => {
   return (
     <header className="header">
       <nav className="nav container">
-        <a href="index.html" className="nav__logo">Ankit</a>
-        <div className="nav__list grid">
-          <div className="nav__item">
+        <a href="index.html" className="nav__logo">
+          Ankit
+        </a>
+        <div className="nav__menu">
+        <ul className="nav__list ">
+          <li className="nav__item">
             <a href="#home" className="nav__link">
-              <i className="uil uil-estate nav__icon">
-
-              </i>Home
+              <i className="uil uil-estate nav__icon"></i>Home
             </a>
-          </div>
-          <div className="nav__item">
+          </li>
+          <li className="nav__item">
             <a href="#about" className="nav__link">
-              <i className="uil uil-user nav__icon">
-
-              </i>
+              <i className="uil uil-user nav__icon"></i>
               About
             </a>
-          </div>
-          <div className="nav__item">
+          </li>
+          <li className="nav__item">
             <a href="#skills" className="nav__link">
-              <i className="uil uil-file-alt  nav__icon">
-
-              </i>
+              <i className="uil uil-file-alt  nav__icon"></i>
               Skills
             </a>
-          </div>
-          <div className="nav__item">
+          </li>
+          <li className="nav__item">
             <a href="#services" className="nav__link">
-              <i className="uil uil-briefcase-alt  nav__icon">
-
-              </i>
+              <i className="uil uil-briefcase-alt  nav__icon"></i>
               Services
             </a>
-          </div>
-          <div className="nav__item">
+          </li>
+          <li className="nav__item">
             <a href="portfolio" className="nav__link">
-              <i className="uil uil-scenery nav__icon">
-
-              </i>
+              <i className="uil uil-scenery nav__icon"></i>
               Portfolio
             </a>
-          </div>
-          <div className="nav__item">
+          </li>
+          <li className="nav__item">
             <a href="#contact" className="nav__link">
-              <i className="uil uil-message nav__icon">
-                
-              </i>
+              <i className="uil uil-message nav__icon"></i>
               Contact
             </a>
-          </div>
+          </li>
+          
+        </ul>
+        <i className="uil uil-times nav__close"></i>
+        </div>
+        <div className="nav__toggle">
+          <i className="uil uil-apps"></i>
         </div>
       </nav>
     </header>
-   
-  )
-}
+  );
+};
 
 export default Header;
