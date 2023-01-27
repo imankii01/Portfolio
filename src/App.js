91924fed0923f1aef2e35e96f3a0374f4bd3460c
Ankit <codewithankit047@gmail.com>
2023-01-27 22:53:52 +0530

    27-jan-2023 11:00pm

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Header from './components/Header/Header'
 import './App.css'
+import { Home } from './components/Home/Home'
 
 
 const App = () => {
   return (
     <>
     <Header/>
+   
+    <main className="main">
+    <Home/>
+    </main>
     </>
   )
 }
