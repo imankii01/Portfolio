fafc09e24096f8d24c568d32291489242101e3b4
Ankit <codewithankit047@gmail.com>
2023-01-28 15:13:08 +0530

    Add own cv

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/Profile_ankit_main.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/Resume-Ankit-Prajapati.pdf";
 import Info from "./Info";
 
 const About = () => {
