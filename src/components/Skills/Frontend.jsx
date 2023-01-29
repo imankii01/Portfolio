4ad9e467e8ca66f0de4caeb464b37b38265534d4
Ankit <codewithankit047@gmail.com>
2023-01-29 21:32:07 +0530

    skills error commit

--- src/components/Skills/Frontend.jsx
+++ src/components/Skills/Frontend.jsx
@@ -2,9 +2,79 @@ import React from 'react'
 
 const Frontend = () => {
   return (
-    <div>
-      
+ <div className="skills__content">
+  <h3 className="skills__title">Frontend Developer</h3>
+  <div className="skills__box">
+    <div className="skills__group">
+    <div className="skills__data">
+      <i className="bx bx-badg-check">
+
+      </i>
+      <div className="">
+        <h3 className="skills__name">HTML</h3>
+        <span className="skills__level">Basic</span>
+      </div>
     </div>
+
+
+    <div className="skills__data">
+      <i className="bx bx-badg-check">
+
+      </i>
+      <div className="">
+        <h3 className="skills__name">CSS</h3>
+        <span className="skills__level">Advacned</span>
+      </div>
+    </div>
+    
+    <div className="skills__data">
+      <i className="bx bx-badg-check">
+
+      </i>
+      <div className="">
+        <h3 className="skills__name">JavaScript</h3>
+        <span className="skills__level">Intermediate</span>
+      </div>
+    </div>
+    
+
+    </div>
+    <div className="skills__group">
+    <div className="skills__data">
+      <i className="bx bx-badg-check">
+
+      </i>
+      <div className="">
+        <h3 className="skills__name">Bootstrap</h3>
+        <span className="skills__level">Intermediate</span>
+      </div>
+    </div>
+
+
+    <div className="skills__data">
+      <i className="bx bx-badg-check">
+
+      </i>
+      <div className="">
+        <h3 className="skills__name">Git</h3>
+        <span className="skills__level">Intermediate</span>
+      </div>
+    </div>
+    
+    <div className="skills__data">
+      <i className="bx bx-badg-check">
+
+      </i>
+      <div className="">
+        <h3 className="skills__name">ReactJS</h3>
+        <span className="skills__level">Intermediate</span>
+      </div>
+    </div>
+    
+
+    </div>
+  </div>
+ </div>
   )
 }
 
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Frontend from './Frontend'
+import Frontend from './Frontend'
 import './skills.css'
 
 const Skills = () => {
@@ -7,7 +9,8 @@ const Skills = () => {
   <h2 className="section__title">Skills</h2>
   <span className="section__subtitle">My technical level</span>
 <div className="skills__container container grid">
-
+<Frontend/>
+<Backend/>
 </div>
 </section>
   )
