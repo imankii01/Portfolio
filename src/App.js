0774260229564365209f7f786dfcdfc9f7d1eb67
Ankit <codewithankit047@gmail.com>
2023-01-30 00:16:07 +0530

    services half completed and error in css

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import { Home } from './components/Home/Home'
 import About from './components/About/About'
 import Skills from './components/Skills/Skills'
+import Services from './components/Services/Services'
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
     <Home/>
     <About/>
     <Skills/>
+    <Services/>
     </main>
     </>
   )
