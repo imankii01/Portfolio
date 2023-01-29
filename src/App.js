2740732ad14312202db62038b14b56467c84d328
Ankit <codewithankit047@gmail.com>
2023-01-29 18:18:06 +0530

    start skills coon

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from './components/Header/Header'
 import './App.css'
 import { Home } from './components/Home/Home'
 import About from './components/About/About'
+import Skills from './components/Skills/Skills'
 
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
     <main className="main">
     <Home/>
     <About/>
+    <Skills/>
     </main>
     </>
   )
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './skills.css'
+
+const Skills = () => {
+  return (
+<section className="skills section" id="skills">
+  <h2 className="section__title">Skills</h2>
+  <span className="section__subtitle">My technical level</span>
+<div className="skills__container container grid">
+
+</div>
+</section>
+  )
+}
+
+export default Skills
